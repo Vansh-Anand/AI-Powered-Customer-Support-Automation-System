76e0b2ed614d3ed83d6b337bd94d1b7bef752389
Vansh-Anand <Vanshanand215@gmail.com>
2026-06-28 12:12:53 +0530

    PROJECT SETUP

---
+++ README.md
@@ -0,0 +1,16 @@
+# AI-Powered Customer Support Automation System
+
+Author: Vansh Anand
+
+Built using:
+
+- LangGraph
+- LangChain
+- Ollama
+- SQLite
+- ChromaDB
+
+Status:
+Project Setup Completed.
+
+We'll expand it throughout the project.
---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+AI-Powered Customer Support Automation System
+Author: Vansh Anand
+"""
+
+def main():
+    print("=" * 60)
+    print("ABC Technologies Customer Support Automation")
+    print("=" * 60)
+
+    print("\nProject setup completed successfully.")
+
+if __name__ == "__main__":
+    main()
